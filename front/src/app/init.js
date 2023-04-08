0fd52b11a3bc775ff0012f5d0f9c3a3ddf70bded
EugeneGpil <gpilligrim@gmail.com>
2023-04-08 19:53:11 +0300

    1

--- front/src/app/init.js
+++ front/src/app/init.js
@@ -7,12 +7,15 @@ export default (container) => {
 
   renderer.setSize(window.innerWidth, window.innerHeight);
 
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+
   container.appendChild(renderer.domElement);
 
   const scene = new THREE.Scene();
 
   const camera = new THREE.PerspectiveCamera(
-    45, //between 40 and 80
+    80, //between 40 and 80
     window.innerWidth / window.innerHeight,
     0.1,
     1000
@@ -26,7 +29,7 @@ export default (container) => {
   // camera.position.z = 5;
   // camera.position.y = 2;
   // camera.position.x = 2;
-  camera.position.set(-3, 10, 10);
+  camera.position.set(-10, 10, 10);
   orbit.update();
 
   const boxGeometry = new THREE.BoxGeometry();
@@ -35,40 +38,64 @@ export default (container) => {
   scene.add(box);
 
   const planeGeometry = new THREE.PlaneGeometry(30, 30);
-  const planeMaterial = new THREE.MeshBasicMaterial({
+  const planeMaterial = new THREE.MeshStandardMaterial({
     color: 0xffffff,
     side: THREE.DoubleSide,
   });
   const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.receiveShadow = true;
   scene.add(plane);
   plane.rotation.x = -0.5 * Math.PI;
 
   const gridHelper = new THREE.GridHelper(30 /*100*/);
   scene.add(gridHelper);
 
-  const sphereGeometry = new THREE.SphereGeometry(4, 16, 8);
-  const sphereMaterial = new THREE.MeshBasicMaterial({
-    color: 0x0000ff,
-    wireframe: false,
-  });
-  // const sphereMaterial = new THREE.MeshStandardMaterial({
-  //   color: 0x0000FF,
+  const sphereGeometry = new THREE.SphereGeometry(2, 16, 8);
+  // const sphereMaterial = new THREE.MeshBasicMaterial({
+  //   color: 0x0000ff,
   //   wireframe: false,
-  // })
+  // });
+  const sphereMaterial = new THREE.MeshStandardMaterial({
+    color: 0x0000FF,
+    wireframe: false,
+  })
   // const sphereMaterial = new THREE.MeshLambertMaterial({
   //   color: 0x0000FF,
   //   wireframe: false,
   // })
   const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphere.castShadow = true;
   scene.add(sphere);
 
   sphere.position.x = -5;
+  sphere.position.y = 7;
+  sphere.position.z = 2;
+
+
+  const ambientLight = new THREE.AmbientLight(0x333333);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+  directionalLight.position.set(-30, 50, 0)
+  directionalLight.castShadow = true;
+  directionalLight.shadow.camera.bottom = -20
+  directionalLight.shadow.camera.left = -20
+  directionalLight.shadow.camera.right = 20
+  directionalLight.shadow.camera.top = 20
+  scene.add(directionalLight)
+
+  const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+  scene.add(directionalLightHelper)
+
+  const directionalLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+  scene.add(directionalLightShadowHelper)
 
   const gui = new dat.GUI();
 
   const options = {
     sphereColor: "#ffee00",
     wireframe: false,
+    speed: 0.01,
   };
 
   gui.addColor(options, "sphereColor").onChange((e) => {
@@ -79,9 +106,17 @@ export default (container) => {
     sphere.material.wireframe = e;
   });
 
+  gui.add(options, "speed", 0, 0.1)
+
+  let step = 0;
+
   renderer.setAnimationLoop((time) => {
     box.rotation.x = time / 2400;
     box.rotation.y = time / 2400;
+
+    step += options.speed;
+    sphere.position.y = 10 * Math.abs(Math.sin(step)) + 2
+
     renderer.render(scene, camera);
   });
 
